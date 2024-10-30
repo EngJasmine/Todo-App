af6a5fdca2943e3403c2251dd41d427f9a850f6d
Yasmine-Nasri <yasmine_nasri@yahoo.fr>
2024-10-30 12:07:28 -0400

    Clean Code:Remove unnecessary comments

--- gui.py
+++ gui.py
@@ -1,13 +1,5 @@
-'''from fileinput import close
-
-import functions
-import FreeSimpleGUI
-FreeSimpleGUI.Window('My To-Do Application')
-FreeSimpleGUI.Window.read()
-FreeSimpleGUI.Window.close()'''
 import FreeSimpleGUI as sg
 # Define the layout
-#layout = [[sg.Text("Type in a to-do"), sg.InputText(tooltip="Enter todo")]]
 label=sg.Text('Type in a to-do')
 input_box=sg.InputText(tooltip='Enter todo')
 add_button=sg.Button('Add')
